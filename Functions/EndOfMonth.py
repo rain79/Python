455f0d574799baeb4591193952637672a33530fb
Frédéric <rain79@gmail.com>
2016-11-19 22:18:14 -0500

    Create EndOfMonth.py

---
+++ Functions/EndOfMonth.py
@@ -0,0 +1,16 @@
+#########################################################################################
+# EndOfMonth.py - Python Script to the last day of the current month.
+# Copyrights (c) - Frédéric Charette 2016
+#
+# usage: Call the function to a variable in order to report the last day of the month
+#
+##########################################################################################
+#
+
+import datetime
+
+def LastDayOfMonth(AnyDay):
+    NextMonth = AnyDay.replace(day=28) + datetime.timedelta(days=4)  
+    return NextMonth - datetime.timedelta(days=NextMonth.day)
+
+dEndOfMonth = LastDayOfMonth(datetime.date.today()) 
